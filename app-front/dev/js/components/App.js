7de30952827cf9aeb5b45a31ee208df4af33b7e1
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-07 23:53:31 +0000

    trying out boilerplate

---
+++ app-front/dev/js/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+require('../../scss/style.scss');
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
