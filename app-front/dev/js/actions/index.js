4247b8ed759bc6e6ab51734261201678844f8fb3
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-08 01:30:24 +0000

    added api url

--- app-front/dev/js/actions/index.js
+++ app-front/dev/js/actions/index.js
@@ -1,10 +1,10 @@
 import {GET_TITLES} from './types'
 import axios from 'axios'
 
-const API_URL = ""
+const API_URL = "https://kitsu.io/api/edge/anime"
 
 export function getTitles(){
-  const request = axios.get(`${API_URL}/titles`)
+  const request = axios.get(`${API_URL}`)
   return {
     type: GET_TITLES,
     payload: request
