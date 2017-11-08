a15a3a2d1a2b5b07c72a9696e014d7de7d586c13
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-08 15:39:54 +0000

    adding to containers

---
+++ veets-client/src/containers/Titles.js
@@ -0,0 +1,61 @@
+import { connect } from 'react-redux';
+import { addTitle } from '../actions/addTitle';
+import React from 'react';
+import MyTitleService from '../services/MyTitleService';
+import { bindActionCreators } from 'redux';
+import TitleCard from '../components/TitleCard';
+import TitleShow from './TitleShow'
+
+class Titles extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      titles: []
+    };
+  }
+
+  componentDidMount() {
+    MyTitleService.fetchTitles()
+    .then(json => json.forEach((title) => {
+      var action = this.props.addTitle(title)
+      console.log(this.props.store.getState())
+    }))
+  }
+
+  render(){
+    try {
+      var titles = this.props.titles.map((title, index) =>
+        <TitleCard name={title.canonicalTitle} summary={title.synopsis} rank={title.popularityRank} episodes={title.episodeCount} rating={title.averageRating} image={title.coverImage.small} data={title}  store={this.props.store} columns={1} />
+      )
+    } catch(err) {
+      console.log(err)
+      var popTitles = ""
+    }
+
+    console.log(this.state.titles)
+    return (
+      <div>
+        <p>Titles </p>
+        <div className="col-sm-6">
+          {titles}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    titles: state.titles,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addTitle: addTitle,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Titles)
