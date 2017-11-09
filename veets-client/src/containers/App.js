fa4bbb88743af9ea48dd67d9c83360c9ba48d51b
msickler <31420607+msickler@users.noreply.github.com>
2017-11-09 13:30:28 -0500

    setting up navbar and more functions

--- veets-client/src/containers/App.js
+++ veets-client/src/containers/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Books from './Books';
+import NavBar from '../components/NavBar'
+import Header from '../components/Header.js'
+import NotFound from '../components/NotFound.js'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Books />
+        <Header />
+          <Router>
+            <div>
+              <NavBar />
+              <Switch>
+                <Route exact path="/" component={Books} />
+                <Route  component={NotFound} />
+              </Switch>
+            </div>
+          </Router>
       </div>
     );
   }
