9c82ced3e0d259febe1fef334b52cff1af3e3bd4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-09 15:05:56 +0000

    deleting files that arent needed

--- veets-client/src/index.js
+++ veets-client/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
-import rootReducer from './reducers/index.js'
 
-const store = createStore(rootReducer);
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import store from './store.js';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store} />
-   </Provider>,
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ veets-client/src/store.js
@@ -0,0 +1,17 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+
+import books from './reducers/books';
+import bookFormData from './reducers/bookFormData';
+
+const reducers = combineReducers({
+  books,
+  bookFormData
+});
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
